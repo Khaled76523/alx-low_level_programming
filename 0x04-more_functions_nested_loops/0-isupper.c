1646c1148dfb013cabe70895057e0ab0eea0f6fc
khaled76523 <luka.salama23@gmail.com>
2023-08-17 19:09:14 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper _ Checks for lowecase character
+ * @c: The character to be checked
+ * Return: 1 for uppercase character or 0 for anything else
+ */
+
+int _isuupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
